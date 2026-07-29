0b68a1f53dcb6c21e5b6ee759b74e4f7aa212ea1
Wang King <99259940+WangKKing3@users.noreply.github.com>
2026-07-29 21:49:56 +0200

    starten av koden
    til neste bygg ferdig def _get_obs(self) funsjonen

--- drone_env.py
+++ drone_env.py
@@ -12,11 +12,40 @@ class FPVDroneRaceEnv(gym.Env):
         super(FPVDroneRaceEnv, self).__init__()
         # Define the action and observation spaces
         self.action_space = spaces.Box(low=-1, high=1, shape=(4,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(12,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(15,), dtype=np.float32)
 
-    def reset(self):
+        self.mass= 0.67             # 670g med batteri )
+        self.max_thrust_N = 62.4    # Maximum thrust in Newtons. (basert på 5 inch 6s drone med 1880kv motor og gemfan Freestyle 3 blade  5226)
+        self.drag_coeff = 0.1       # Drag coefficient
+        self.dt = 0.02              # 50 Hz simuleringsfrekvens  
+
+        # Define the race track layout (gates positions
+        self.gates = np.array([
+            [10.0,  0.0, 2.0],
+            [20.0, 10.0, 3.5],
+            [10.0, 20.0, 2.0],
+            [ 0.0, 10.0, 1.5]
+        ], dtype=np.float32)
+
+        self.gate_radius = 2.5  # Radius of the gates for collision detection 2.5 meter
+
+        self.reset()
+
+    def reset(self, seed=None, options=None):
         # Reset the environment and return the initial observation
-        pass
+        super().reset(seed=seed)
+
+        # Initialize the drone's state
+        self.pos = np.array([0.0, 0.0, 1.0], dtype=np.float32)  
+        self.vel = np.array([0.0, 0.0, 0.0], dtype=np.float32)
+        self.angles = np.array([0.0, 0.0, 0.0], dtype=np.float32)
+        self.ang_vel = np.array([0.0, 0.0, 0.0], dtype=np.float32)
+
+        self.current_gate_index = 0
+        self.step = 0
+        self.mac_steps = 1500 
+
+        return self._get_observation(), {}
 
     def step(self, action):
         # Execute one time step within the environment
